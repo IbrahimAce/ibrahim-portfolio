1dc4e57d0d08b14304f06c72c8c1c0683e636ed0
John Doe <example@gmail.com>
2026-03-20 18:19:54 +0300

    Fix database import

--- ibrahim_portfolio/settings.py
+++ ibrahim_portfolio/settings.py
@@ -55,7 +55,8 @@ WSGI_APPLICATION = 'ibrahim_portfolio.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR}/db.sqlite3',
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=False
     )
 }
 
