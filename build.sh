6843db19bf17ffcdd8c69b6c04d43e11426a7982
John Doe <example@gmail.com>
2026-03-20 18:58:56 +0300

    Auto create superuser on deploy

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='ibrahim').exists() or User.objects.create_superuser('ibrahim', 'karanjaibrahim141@gmail.com', 'Admin@2026!')" | python manage.py shell
